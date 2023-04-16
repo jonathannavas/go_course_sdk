51c2e625ccb16906512868d6eb4718cb4860685f
Jonathan <jonathan.navas9@hotmail.com>
2023-04-16 18:47:02 -0500

    fix handle error messages

--- course/transport.go
+++ course/transport.go
@@ -47,14 +47,16 @@ func (c *clientHTTP) Get(id string) (*domain.Course, error) {
 	if reps.Err != nil {
 		return nil, reps.Err
 	}
-	if reps.StatusCode == 404 {
-		return nil, ErrNotFound{fmt.Sprintf("%s", reps)}
-	}
-	if reps.StatusCode > 299 {
-		return nil, fmt.Errorf("%s", reps)
-	}
+
 	if err := reps.FillUp(&dataResponse); err != nil {
 		return nil, err
+	}
+
+	if reps.StatusCode == 404 {
+		return nil, ErrNotFound{fmt.Sprintf("%s", dataResponse.Message)}
+	}
+	if reps.StatusCode > 299 {
+		return nil, fmt.Errorf("%s", dataResponse.Message)
 	}
 	return dataResponse.Data.(*domain.Course), nil
 }
